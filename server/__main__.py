f16eeaed3d02d3704bd8e43fc1dcd2bc35ad61cb
spinagon <890571+spinagon@users.noreply.github.com>
2022-07-30 13:50:43 +0300

    Run server with "python -m server"

--- server/__main__.py
+++ server/__main__.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,3 +8,7 @@ app = FastAPI()
 async def root():
     """Main page of our server, this is not a web-based applications, so should just show some information on usage"""
     return {"message": "This is a server for picphone app (Python Discord Summer Code Jam 2022)"}
+
+
+if __name__ == '__main__':
+    uvicorn.run("__main__:app", host="0.0.0.0", port=5000)
